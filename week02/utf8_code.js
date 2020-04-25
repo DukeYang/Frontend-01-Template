2ae4b741a3717e48e99bd470da349b6049677918
DukeYang <duke_yang@163.com>
2020-04-25 16:28:37 +0800

    Update utf8_code.js

--- week02/utf8_code.js
+++ week02/utf8_code.js
@@ -1,1 +1,15 @@
+function utf8_decode(str) {
+    let res = ""
+    for (const char in str) {
+        if (str.hasOwnProperty(char)) {
+            const element = char_utf8(str[char]);
+            res += element
+        }
+    }
+    return res
+}
 
+function char_utf8(ch) {
+    utf8 = ch.charCodeAt().toString(16)
+    return "\\u"+utf8
+}
