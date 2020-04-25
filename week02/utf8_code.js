1a176bef09edbbc4c29e39bc246ec9a6e0929502
DukeYang <duke_yang@163.com>
2020-04-25 19:06:38 +0800

    Update utf8_code.js

--- week02/utf8_code.js
+++ week02/utf8_code.js
@@ -9,7 +9,13 @@ function utf8_decode(str) {
     return res
 }
 
-function char_utf8(ch) {
-    utf8 = ch.charCodeAt().toString(16)
-    return "\\u"+utf8
+function char_utf8( ch ) {
+    let bits = ch.codePointAt().toString(2)
+    if ( bits.length < 8 ) return bits.padStart(8, '0');
+    const cnt = Math.ceil((bits.length - 1) / 5)
+    
+    bits = bits.replace(/(?=(\B)([01]{6})+$)/g, '10');
+    bits = `${'1'.repeat(byteCount)}${bits.padStart(7 * byteCount, '0')}`;
+    return bits.replace(/(?=(\B)([01]{8})+$)/g, '|');
+    // return "\\u"+utf8
 }
